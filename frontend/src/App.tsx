780d7c8719c426380e1f11ce18097b143001def7
Lukáš Peťko <luke.petko@gmail.com>
2024-03-02 19:31:10 +0100

    template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import logo from "./assets/images/logo-universal.png";
+import "./App.css";
+import { Greet, Test } from "../wailsjs/go/main/App";
+
+function App() {
+  const [resultText, setResultText] = useState(
+    "Please enter your name below 👇",
+  );
+  const [name, setName] = useState("");
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
+
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
+
+  function test() {
+    Test().then(console.log);
+  }
+
+  return (
+    <div id="App">
+      <img src={logo} id="logo" alt="logo" />
+      <div id="result" className="result">
+        {resultText}
+      </div>
+      <div id="input" className="input-box">
+        <input
+          id="name"
+          className="input"
+          onChange={updateName}
+          autoComplete="off"
+          name="input"
+          type="text"
+        />
+        <button className="btn" onClick={test}>
+          Greet
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
