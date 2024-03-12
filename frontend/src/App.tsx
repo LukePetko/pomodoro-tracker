b52c49bd54e7e479751440f238e9472d667cc41e
Lukáš Peťko <luke.petko@gmail.com>
2024-03-12 20:56:15 +0100

    adding testing button to play audio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useRef } from "react";
 import Settings from "./components/settings/Settings";
 import Timer from "./components/timer/Timer";
-import { notify } from "./lib/notifications";
+import { Button } from "./components/ui/button";
 
 function App() {
+  const audioRef = useRef<HTMLAudioElement>(null);
   return (
     <>
       <div className="draggable fixed top-0 flex h-10 w-screen justify-end p-3">
@@ -11,6 +13,19 @@ function App() {
       <div className="pt-10">
         <Timer />
       </div>
+      <audio
+        id="audio"
+        src="https://github.com/LukePetko/pomodoro-tracker/blob/main/frontend/src/assets/notify.mp3?raw=true"
+        preload="auto"
+        ref={audioRef}
+      />
+      <Button
+        onClick={() => {
+          audioRef.current?.play();
+        }}
+      >
+        play
+      </Button>
     </>
   );
 }
